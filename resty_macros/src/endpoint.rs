d873372609feb8f77638f43c98d22068753d64bc
Shy <shyguyx1412@gmail.com>
2026-07-09 12:28:50 +0200

    finally refactor handler impl

--- resty_macros/src/endpoint.rs
+++ resty_macros/src/endpoint.rs
@@ -3,26 +3,18 @@ use proc_macro_argue::{ArgumentList, argue};
 use quote::{ToTokens, format_ident};
 use syn::spanned::Spanned;
 
-use crate::*;
+use crate::{endpoint::HandlerArgument::Method, *};
 
 argue! {
-    MiddlewareArgument {
-        Router: syn::Path,
-        Route: syn::LitStr,
-        Header: HeaderArgument,
-    };
     HandlerArgument {
         Method: ArgumentList<syn::Expr>,
         Router: syn::Path,
         Route: syn::LitStr,
         Header: HeaderArgument,
-        Accepts: syn::Path,
-        Responds: RespondsArgument,
-        Summary: syn::LitStr,
-        Description: syn::LitStr,
+
+        Meta: ArgumentList<syn::MetaList>,
     };
     HeaderArgument(syn::LitStr, syn::token::Comma, syn::LitStr);
-    RespondsArgument(syn::LitInt, syn::token::Comma, syn::Path);
 }
 
 impl ToTokens for HeaderArgument {
@@ -37,133 +29,95 @@ pub fn endpoint_macro_impl(
     args: TokenStream,
     body: TokenStream,
 ) -> Result<TokenStream, syn::Error> {
+    handler_impl(args, body, endpoint_variant)
+}
+pub fn middleware_macro_impl(
+    args: TokenStream,
+    body: TokenStream,
+) -> Result<TokenStream, syn::Error> {
+    handler_impl(args, body, middleware_variant)
+}
+
+fn handler_impl(
+    args: TokenStream,
+    body: TokenStream,
+    variant: fn(&ArgumentList<HandlerArgument>, &syn::Ident) -> Result<syn::Expr, syn::Error>,
+) -> Result<TokenStream, syn::Error> {
     use HandlerArgument::*;
+    let handler_fn = validate_handler(syn::parse(body)?)?;
+    let fn_ident = &handler_fn.sig.ident;
+    let slice_ident = format_ident!("__{fn_ident}_route");
 
     let args: ArgumentList<HandlerArgument> = syn::parse(args)?;
+    let router = argue!(args may have Router)?
+        .map(|(.., value)| value.clone())
+        .map_or_else(routing::default_router, Ok)?;
 
     let headers: Vec<_> = argue!(args may repeat Header)
         .map(|(.., header)| header)
         .collect();
 
-    let router = argue!(args may have Router)?
-        .map(|(.., value)| value.clone())
-        .map_or_else(routing::default_router, Ok)?;
-
     let route = argue!(args may have Route)?
-        .map(|(.., value)| value.value().split("/").map(ToString::to_string).collect())
+        .map(|(.., value)| value.value())
+        .as_ref()
+        .map(|v| v.strip_prefix("/").unwrap_or(v))
+        .map(|v| v.strip_suffix("/").unwrap_or(v))
+        .map(|v| v.split("/").map(ToString::to_string).collect())
         .map_or_else(routing::default_route, Ok)?;
 
-    let methods = argue!(args must have Method)?.1.into_iter();
+    let variant = variant(&args, fn_ident)?;
+
+    Ok(quote::quote! {
+        pub fn #fn_ident <'a, 'data, '__fn_borrow> (
+            req: &'__fn_borrow mut ::resty::Request<'a, 'data>,
+            res: &'__fn_borrow mut ::resty::Response<'a>
+        ) -> ::resty::EndpointTask<'__fn_borrow> {
+            #handler_fn;
+
+            const STATIC_HEADERS :&[(&str, &str)] = &[#((#headers)),*];
+
+            Box::pin(async move {
+                #fn_ident(req, res).await?;
+                Ok(())
+            })
+        }
+        use ::resty::__private::*;
+        #[linkme::distributed_slice(#router)]
+        #[linkme(crate = linkme)]
+        static #slice_ident: ::resty::RouteSlice =(&[#(#route),*], #variant);
+    }
+    .into())
+}
+
+fn endpoint_variant(
+    args: &ArgumentList<HandlerArgument>,
+    fn_ident: &syn::Ident,
+) -> Result<syn::Expr, syn::Error> {
+    use HandlerArgument::*;
+    let method_arg = argue!(args must have Method)?;
+    let methods = method_arg.1.into_iter();
+
     let method_byte: syn::Expr = syn::parse2(quote::quote! {
         {
             use ::resty::HttpMethod::*;
             #(#methods as u16 )|*
         }
     })?;
-
-    let responds = argue!(args may repeat Responds)
-        .map(|(.., RespondsArgument(code, _, ty))| {
-            syn::parse2::<syn::ExprBlock>(quote::quote! {{
-                fn __doc_validate<T: ::resty::__private::Public>(code: u16) {
-                    __doc_validate::<#ty>(#code);
-                }
-            }})
-        })
-        .ok()?;
-
-    let accepts = argue!(args may repeat Accepts)
-        .map(|(.., ty)| {
-            syn::parse2::<syn::ExprBlock>(quote::quote! {{
-                fn __doc_validate<T: ::resty::__private::Public>() {
-                    __doc_validate::<#ty>();
-                }
-            }})
-        })
-        .ok()?;
-
-    let handler_fn = validate_handler(syn::parse(body)?)?;
-    let fn_ident = &handler_fn.sig.ident;
-    let slice_ident = format_ident!("__{fn_ident}_route");
-    let generics = &handler_fn.sig.generics;
-    let lifetime = generics
-        .lifetimes()
-        .nth(0)
-        .expect("guranteed by validate_handler");
-
-    Ok(quote::quote! {
-        pub fn #fn_ident <#lifetime, '__fn_borrow> (
-            req: &'__fn_borrow mut ::resty::Request<#lifetime>,
-            res: &'__fn_borrow mut ::resty::Response<#lifetime>
-        ) -> ::resty::EndpointTask<'__fn_borrow> {
-            #handler_fn;
-
-            #(#responds)*
-            #(#accepts)*
-
-            const STATIC_HEADERS :&[(&str, &str)] = &[#((#headers)),*];
-
-            Box::pin(async move {
-                #fn_ident(req, res).await?;
-                Ok(())
-            })
-        }
-        use ::resty::__private::*;
-        #[linkme::distributed_slice(#router::__RESTY__ROUTER)]
-        #[linkme(crate = linkme)]
-        static #slice_ident: ::resty::RouteSlice =(&[#(#route),*], ::resty::Handler(&#fn_ident, #method_byte));
-    }
-    .into())
+    syn::parse2(quote::quote! {::resty::Handler(&#fn_ident, #method_byte)})
 }
 
-pub fn middleware_macro_impl(
-    args: TokenStream,
-    body: TokenStream,
-) -> Result<TokenStream, syn::Error> {
-    use MiddlewareArgument::*;
-
-    let args: ArgumentList<MiddlewareArgument> = syn::parse(args)?;
-
-    let router = argue!(args may have Router)?
-        .map(|(.., value)| value.clone())
-        .map_or_else(routing::default_router, Ok)?;
-
-    let route = argue!(args may have Route)?
-        .map(|(.., value)| value.value().split("/").map(ToString::to_string).collect())
-        .map_or_else(routing::default_route, Ok)?;
-
-    let headers: Vec<_> = argue!(args may repeat Header)
-        .map(|(.., header)| header)
-        .collect();
-
-    let handler_fn = validate_handler(syn::parse(body)?)?;
-    let fn_ident = &handler_fn.sig.ident;
-    let slice_ident = format_ident!("__{fn_ident}_route");
-    let generics = &handler_fn.sig.generics;
-    let lifetime = generics
-        .lifetimes()
-        .nth(0)
-        .expect("guranteed by validate_handler");
-
-    Ok(quote::quote! {
-        pub fn #fn_ident <#lifetime, '__fn_borrow> (
-            req: &'__fn_borrow mut ::resty::Request<#lifetime>,
-            res: &'__fn_borrow mut ::resty::Response<#lifetime>
-        ) -> ::resty::EndpointTask<'__fn_borrow> {
-            #handler_fn;
-
-            const STATIC_HEADERS :&[(&str, &str)] = &[#((#headers)),*];
-
-            Box::pin(async move {
-                #fn_ident(req, res).await?;
-                Ok(())
-            })
-        }
-        use ::resty::__private::*;
-        #[linkme::distributed_slice(#router::__RESTY__ROUTER)]
-        #[linkme(crate = linkme)]
-        static #slice_ident: ::resty::RouteSlice =(&[#(#route),*], ::resty::Middleware(&#fn_ident));
+fn middleware_variant(
+    args: &ArgumentList<HandlerArgument>,
+    fn_ident: &syn::Ident,
+) -> Result<syn::Expr, syn::Error> {
+    if let Some((ident, ..)) = argue!(args may have Method)? {
+        return Err(syn::Error::new_spanned(
+            ident,
+            "Method may not be declared for middlewares",
+        ));
     }
-    .into())
+
+    syn::parse2(quote::quote! {::resty::Middleware(&#fn_ident)})
 }
 
 fn validate_handler(mut handler: syn::ItemFn) -> Result<syn::ItemFn, syn::Error> {
@@ -174,16 +128,6 @@ fn validate_handler(mut handler: syn::ItemFn) -> Result<syn::ItemFn, syn::Error>
         .iter()
         .map(|arg| arg.reparse::<syn::PatType>())
         .ok()?;
-
-    let lifetime = handler
-        .sig
-        .generics
-        .lifetimes()
-        .nth(0)
-        .ok_or(syn::Error::new_spanned(
-            handler.sig.clone(),
-            "Expected a lifetime parameter",
-        ))?;
 
     let Some((req, res)) = args.get(0).zip(args.get(1)) else {
         return Err(syn::Error::new_spanned(
@@ -217,8 +161,8 @@ fn validate_handler(mut handler: syn::ItemFn) -> Result<syn::ItemFn, syn::Error>
         quote::quote_spanned! {span => {
             {
                 let __typecheck_handler = async ||{let _: ::resty::Result = #handler_ident(#req_ident, #res_ident).await;};
-                let __typecheck_request: &mut ::resty::Request<#lifetime> = #req_ident;
-                let __typecheck_response: &mut ::resty::Response<#lifetime> = #res_ident;
+                let __typecheck_request: &mut ::resty::Request = #req_ident;
+                let __typecheck_response: &mut ::resty::Response = #res_ident;
             }
             #(#statements)*
         }}
